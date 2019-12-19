85ea0869038c7a0a1d3ec94d212f8870883ed4ec
Omar Emad <omaremadsabry@gmail.com>
2019-12-19 14:48:37 +0200

    [-] Removed NodeJS app files

--- Compensation-Backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express                 = require('express');
-var router                  = express.Router();
-var CompensationsController = require('../controllers/CompensationsController');
-
-/* GET home page. */
-router.get('/getCompensations', CompensationsController.getSlotsToBeCompensated);
-
-module.exports = router;
--- Compensation-Backend/utils/Constants.js
+++
@@ -1,4 +0,0 @@
-module.exports =
-{
-    CALENDAR_EVENTS : {HOLIDAY: 'holiday'}
-}
