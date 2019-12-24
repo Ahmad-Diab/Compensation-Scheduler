087a8b3b64a73677c5939cb1969a9ba5ecce10e6
a7mediab97 <a7mediab97.com>
2019-12-24 17:14:13 +0200

    modify prolog file

--- compensation_backend/compensationapp/compensation_scheduler.pl
+++ compensation_backend/compensationapp/compensation_scheduler.pl
@@ -1,14 +1,14 @@
 :- use_module(library(clpfd)).
 
-% (StaffID , Semester , Course , Tutorials , RoomType ,DaysOFF,RoomSlot , Score)
+% (StaffID , Semester , Course , Tutorials , RoomType ,DaysOFF,RoomSlot )
 
-solve(Schedules , FreeRooms , Preferable , NonPreferable):-
+solve(Schedules , FreeRooms , Preferable , NonPreferable , S):-
+	get_RoomSlots_Var(Schedules , RoomSlots),
 	free_room_constraint(Schedules , FreeRooms),
+	all_distinct(RoomSlots),
 	same_RoomType_constraint(Schedules),
 	group_slot_constraint(Schedules),
 	staff_slot_constraint(Schedules),
-	get_RoomSlots(Schedules , RoomSlots),
-	all_distinct(RoomSlots),
 	calculatePrefrences(Schedules , Preferable , NonPreferable , Score),
 	labeling([min(Score)] , RoomSlots).
 			
@@ -31,68 +31,40 @@ free_room_constraint(Schedules , FreeRooms):-
 		all_in_List(RoomSlots , FreeRooms).
 		
 calculatePrefrences(Schedules ,Preferable , NonPreferable, Score):-
-		calculate_preferable(Schedules , Preferable , Score1),
-		calculate_non_preferable(Schedules , NonPreferable , Score2),
-		calculate_not_mentioned(Schedules , Preferable , NonPreferable , Score3),
-		calculate_days_off(Schedules ,Score4) , 
-		Score #= Score1 + Score2 + Score3 + Score4.
+		calculatePrefrences_helper(Schedules , Preferable , 1 , Score1),
+		calculatePrefrences_helper(Schedules , NonPreferable , 2 , Score2),
+		calculate_days_off(Schedules  , Score3),
+		Score #=  Score1 + Score2 + Score3.
 		
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% HELPER Predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%Preferable%%
-calculate_preferable([] , Preferable , 0).
-calculate_preferable([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T] , Preferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	element(_,Preferable , Hash),
-	calculate_preferable(T , Preferable , Score1),
-	Score #= Score1 + 1.
-calculate_preferable([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T] , Preferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	\+ element(_,Preferable , Hash),
-	calculate_preferable(T , Preferable , Score).
-	
+compare_preference((StaffID , Semester , Course , Tutorials , RoomType, DaysOFF, RoomSlot) , Pref , B) :- 
+	Hash #= StaffID * 30 + (RoomSlot mod 30) , 
+	Pref #= Hash #<==> B.
 
-calculate_not_mentioned([] ,Preferable, NonPreferable , 0).
-calculate_not_mentioned([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T]  , Preferable, NonPreferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	(element(_,NonPreferable , Hash); element(_ , Preferable , Hash)),
-	calculate_not_mentioned(T , Preferable, NonPreferable , Score).
+compare_days_off(RoomSlot , Day , B) :- 
+	Hash #= (RoomSlot // 5)  mod 6 ,
+	Day #= Hash #<==> B.
 
-calculate_not_mentioned([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T]  , Preferable, NonPreferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	\+ element(_,NonPreferable , Hash), \+ element(_ , Preferable , Hash),
-	calculate_not_mentioned(T ,Preferable, NonPreferable , Score1),
-	Score #= Score1 + 2.
+calculatePrefrences_helper([] , Prefrences, Factor , 0).
 
-calculate_non_preferable([] , NonPreferable , 0).
-calculate_non_preferable([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T] , NonPreferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	\+ element(_,NonPreferable , Hash),
-	calculate_non_preferable(T , NonPreferable , Score).
-	
-calculate_non_preferable([(StaffID , _ , _ , _ , _ ,_,RoomSlot) | T] , NonPreferable, Score):-
-	Slot #= RoomSlot mod 30 , 
-	Hash #= StaffID * 200 + Slot , 
-	element(_,NonPreferable , Hash),
-	calculate_non_preferable(T , NonPreferable , Score1),
-	Score #= Score1 + 3.
+calculatePrefrences_helper([Schedule | T] , Prefrences, Factor , Score):-
+	maplist(compare_preference(Schedule) , Prefrences , TotalCosts),
+	sum(TotalCosts , #= , Cost), 
+	calculatePrefrences_helper(T , Prefrences , Factor, Score1),
+	Score #= Score1 + Cost * Factor.
 	
 calculate_days_off([] , 0).
-calculate_days_off([(_ , _ , _ , _ , _ ,DaysOFF,RoomSlot) | T] , Score):- 
-		Day #= (RoomSlot // 5)  mod 6, 
-		element(_ ,DaysOFF , Day) , 
-		calculate_days_off(T , Score1) , 
-		Score #= Score1 + 4. 
-calculate_days_off([(_ , _ , _ , _ , _ ,DaysOFF,RoomSlot) | T] , Score):- 
-		Day #= (RoomSlot // 5)  mod 6, 
-		\+ element(_ ,DaysOFF , Day) , 
-		calculate_days_off(T , Score). 
-		
+calculate_days_off([Schedule | T]  , Score):- 
+	Schedule = (StaffID , Semester , Course , Tutorials , RoomType, DaysOFF, RoomSlot), 
+	maplist(compare_days_off(RoomSlot) , DaysOFF , TotalCosts),
+	sum(TotalCosts , #= , Cost), 
+	calculate_days_off(T  , Score1),
+	Score #= Score1 + Cost * 3.
+	
+get_schedule_var(Schedules , SchedulesVar):- 
+	setof((StaffID , Semester , Course , Tutorials , RoomType ,DaysOFF , RoomSlot) , StaffID^Semester ^ Course^ Tutorials^ RoomType^ RoomSlot^DaysOFF^(member((StaffID , Semester , Course , Tutorials , RoomType ,DaysOFF , RoomSlot) , Schedules) , nonvar(RoomSlot)) , RoomSlots).
+			
 get_RoomSlots(Schedules , RoomSlots):- 
 	bagof(RoomSlot , StaffID^Semester ^ Course^ Tutorials^ RoomType^ RoomSlot^DaysOFF^ member((StaffID , Semester , Course , Tutorials , RoomType, DaysOFF, RoomSlot) , Schedules), RoomSlots).
 
